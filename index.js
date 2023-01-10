e9e5628ca8ff46df6ad55d8afa7f1aadd04e72ee
Arsenii Likhachev <mac452ares@gmail.com>
2023-01-10 22:39:17 +0700

    feat: init eslint-config package

---
+++ index.js
@@ -0,0 +1,119 @@
+module.exports = {
+  env: {
+    es6: true,
+    browser: true,
+    node: true,
+  },
+  reportUnusedDisableDirectives: true,
+  extends: [
+    'eslint:recommended',
+    'plugin:vue/vue3-recommended',
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest'
+  },
+  rules: {
+    'semi': ['error', 'never'],
+    'quotes': ['error', 'single'],
+    'quote-props': ['error', 'consistent-as-needed'],
+    'no-unused-vars': 'warn',
+    'no-param-reassign': 'off',
+    'array-bracket-spacing': ['error', 'never'],
+    'brace-style': ['error', 'stroustrup', { allowSingleLine: true }],
+    'block-spacing': ['error', 'always'],
+    'camelcase': ['error', { 'properties': 'always' }],
+    'comma-spacing': ['error', { before: false, after: true }],
+    'comma-style': ['error', 'last'],
+    'comma-dangle': ['error', 'always-multiline'],
+    'no-constant-condition': 'warn',
+    'no-debugger': 'error',
+    'no-console': ['warn', { allow: ['warn', 'error'] }],
+    'no-cond-assign': ['error', 'always'],
+    'func-call-spacing': ['off', 'never'],
+    'key-spacing': ['error', { beforeColon: false, afterColon: true }],
+    'indent': ['error', 2],
+    'no-restricted-syntax': [
+      'error',
+      'DebuggerStatement',
+      'LabeledStatement',
+      'WithStatement',
+    ],
+    'object-curly-spacing': ['error', 'always'],
+    'no-return-await': 'off',
+    'space-before-function-paren': ['error', 'always'],
+
+    'no-var': 'error',
+    'prefer-const': [
+      'error',
+      {
+        destructuring: 'all',
+        ignoreReadBeforeAssign: true,
+      },
+    ],
+    'prefer-arrow-callback': ['error'],
+    'object-shorthand': [
+      'error',
+      'always',
+      {
+        ignoreConstructors: false,
+        avoidQuotes: true,
+      },
+    ],
+    'prefer-exponentiation-operator': 'error',
+    'prefer-rest-params': 'error',
+    'prefer-spread': 'error',
+    'prefer-template': 'error',
+    'template-curly-spacing': 'error',
+    'arrow-parens': ['error', 'as-needed', { requireForBlockBody: true }],
+    'generator-star-spacing': 'off',
+    'spaced-comment': [
+      'error', 
+      'always', 
+      {
+        line: {
+          markers: ['/'],
+          exceptions: ['/', '#'],
+        },
+        block: {
+          markers: ['!'],
+          exceptions: ['*'],
+          balanced: true,
+        },
+      }
+    ],
+    'no-use-before-define': ['error', { functions: false, classes: false, variables: true }],
+
+    'array-callback-return': 'error',
+    'block-scoped-var': 'error',
+    'consistent-return': 'off',
+    'complexity': ['off', 11],
+    'eqeqeq': ['error', 'smart'],
+    'no-alert': 'warn',
+    'no-case-declarations': 'error',
+    'no-multi-spaces': 'error',
+    'no-multi-str': 'error',
+    'no-with': 'error',
+    'no-void': 'error',
+    'no-useless-escape': 'off',
+    'vars-on-top': 'error',
+    'require-await': 'off',
+    'no-return-assign': 'off',
+    'operator-linebreak': ['error', 'before'],
+    'max-statements-per-line': ['error', { max: 1 }],
+
+    'sort-imports': [
+      'error',
+      {
+        ignoreCase: false,
+        ignoreDeclarationSort: true,
+        ignoreMemberSort: false,
+        memberSyntaxSortOrder: ['none', 'all', 'multiple', 'single'],
+        allowSeparatedGroups: false,
+      },
+    ],
+
+    'vue/script-indent': ['error', 2],
+    'vue/html-indent': ['error', 2],
+    'vue/component-name-in-template-casing': ['error', 'PascalCase'],
+  },
+}
